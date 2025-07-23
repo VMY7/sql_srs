a6031e76b9cda74f32564f41a7eca2d8d2632ebe
VMY <vincent.my@hotmail.fr>
2025-07-23 16:07:59 +0200

    modify first commit

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import duckdb as db
+import duckdb
 
 st.write("Hello world!")
 data = {"a": [1, 2, 3], "b": [4, 5, 6]}
@@ -11,5 +11,5 @@ tab1, tab2, tab3 = st.tabs(["Cats", "Dogs", "Owls"])
 with tab1:
     st.dataframe(df)
     input_text = st.text_input(label="Enter a text")
-    st.write(db.sql(input_text))
+    st.write(duckdb.sql(input_text))
 

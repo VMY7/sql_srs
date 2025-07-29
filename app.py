9849a3c794e5b74f725184de19ccd0ed4cee42a1
VMY <vincent.my@hotmail.fr>
2025-07-29 17:13:29 +0200

    display only available theme in select box

--- app.py
+++ app.py
@@ -16,21 +16,22 @@ if "exercises_sql_tables.duckdb" not in os.listdir("data"):
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
 with st.sidebar:
+    available_themes_df = con.execute("SELECT DISTINCT theme FROM memory_state").df()
     theme = st.selectbox(
         "What would you like to review ?",
-        ["cross_joins", "Group By", "window_functions"],
+        available_themes_df["theme"].unique(),
         index=None,
         placeholder="Select a theme...",
     )
 
     if theme:
         st.write("You selected:", theme)
-        select_exercise_query = f"SELECT * FROM memory_state WHERE theme = '{theme}'"
+        SELECT_EXERCISE_QUERY = f"SELECT * FROM memory_state WHERE theme = '{theme}'"
     else:
-        select_exercise_query = f"SELECT * FROM memory_state"
+        SELECT_EXERCISE_QUERY = "SELECT * FROM memory_state"
 
     exercise = (
-        con.execute(select_exercise_query)
+        con.execute(SELECT_EXERCISE_QUERY)
         .df()
         .sort_values("last_reviewed")
         .reset_index(drop=True)
